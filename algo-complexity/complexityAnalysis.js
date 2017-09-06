177d0366a2cdcd0f65507d357f1b4cf6d2f73ffc
ammarRBK <alshakmaskeain@gmail.com>
2017-09-06 12:05:11 +0300

    hahaha

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,14 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
-
+there is several typres of time complexity in algorithmic:
+1-big O(1):'forexample' if we reached to an element inside an array directly without itereating throw
+all elements we used big O(1).
+2- O(n):'forexample' if we iteareted throw all elements in an array to search for some elements we want to
+emplement somthing on it.
+3-O(n)2:'forexample' if we iteareted throw all elements in an array to search for some elements we want to
+emplement somthing on it and then iteareted another time we iteareted throw the same array we used O(n)2.
+log O(n):'forwxample' if we search for one element in an array and we searched part by part from the array 
+like the tree way when we cut the array to two parts and and if we did not find the element we leave this 
+part and move to the other part. 
 */
